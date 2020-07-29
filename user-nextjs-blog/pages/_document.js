c77b5613a6a1c08e28a22f21cc18da34acd931e1
MayPhyoKyaw <66823977+MayPhyoKyaw@users.noreply.github.com>
2020-07-29 09:56:24 +0630

    Update _document.js

--- user-nextjs-blog/pages/_document.js
+++ user-nextjs-blog/pages/_document.js
@@ -11,17 +11,21 @@ export default class MyDocument extends Document {
 
             <link rel="stylesheet" href="/styles/user.css" />
 
+            <link rel="stylesheet" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.css" />
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css"></link>
             <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" />
             <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
-
+            
             <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
             <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
             <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-
+            <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+            <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
             <script src="/__/firebase/7.16.0/firebase-app.js"></script>
             <script src="/__/firebase/7.16.0/firebase-analytics.js"></script>
             <script src="/__/firebase/init.js"></script>
+
         </Head>
         <body>
           <Main></Main>
